3056d33bf769f116926df1dd101f8ff7a47ba4c8
Adalberto Sampaio <sampaio.afn@gmail.com>
2022-04-09 13:21:30 -0300

    feat: Creates user model

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      maxLength: 12,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        'Please enter a valid e-mail',
+      ],
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timeStamps: true,
+  }
+);
+
+module.exports = model('User', userSchema);
